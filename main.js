0a010aa3c4b1565807634626ef3385b44ab1284a
Heather Gallop <doyouseeitmyway@gmail.com>
2019-11-11 12:02:24 -0500

    The Vue Instance
    Demonstrate how to use Vue to display data onto a webpage.

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var product = "Socks";
+// Creates a new Vue instance
+// Options object {} is passed in to store data and perform actions
+var app = new Vue({
+    // property el = Element, used to tell the instance where to plug in
+    el: '#app',
+    // property to store data
+    // data from this instance is linked to every place it is referenced
+    data: {
+        product: 'Socks',
+        description: 'Warm and woolen.'
+    }
+});
